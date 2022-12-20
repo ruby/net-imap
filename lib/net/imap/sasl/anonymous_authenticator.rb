663147bdf8e6d4617330b36c3355ac4545665a1a
nick evans <nicholas.evans@gmail.com>
2022-12-20 09:46:38 -0500

    📚 Minor updates for ANONYMOUS SASL rdoc

--- lib/net/imap/sasl/anonymous_authenticator.rb
+++ lib/net/imap/sasl/anonymous_authenticator.rb
@@ -21,7 +21,7 @@ module Net
         # #anonymous_message is an optional message which is sent to the server.
         # It may be sent as a positional argument or as a keyword argument.
         #
-        # Any other keyword parameters are quietly ignored.
+        # Any other keyword arguments are silently ignored.
         def initialize(anon_msg = nil, anonymous_message: nil, **)
           message = (anonymous_message || anon_msg || "").to_str
           @anonymous_message = StringPrep::Trace.stringprep_trace message
@@ -31,14 +31,15 @@ module Net
           end
         end
 
-        # A token sent for the +ANONYMOUS+ mechanism.
+        # An optional token sent for the +ANONYMOUS+ mechanism., up to 255 UTF-8
+        # characters in length.
         #
         # If it contains an "@" sign, the message must be a valid email address
         # (+addr-spec+ from RFC-2822[https://tools.ietf.org/html/rfc2822]).
         # Email syntax is _not_ validated by AnonymousAuthenticator.
         #
         # Otherwise, it can be any UTF8 string which is permitted by the
-        # StringPrep::Trace profile, up to 255 UTF-8 characters in length.
+        # StringPrep::Trace profile.
         attr_reader :anonymous_message
 
         # :call-seq:
@@ -48,7 +49,9 @@ module Net
         def initial_response?; true end
 
         # Returns #anonymous_message.
-        def process(_server_challenge_string) anonymous_message end
+        def process(_server_challenge_string)
+          anonymous_message
+        end
 
       end
     end
