62630b5551f3665c2c0451603eb91f333750f1ff
nicholas evans <nicholas.evans@gmail.com>
2023-02-02 00:50:24 -0500

    ✨ Make text in resp-text optional (IMAP4rev2)
    RFC3501 (IMAP4rev1):
      resp-text       = ["[" resp-text-code "]" SP] text
    
    RFC9051 (IMAP4rev2):
      resp-text       = ["[" resp-text-code "]" SP] [text]
    
    And in Appendix E:
       23.  resp-text ABNF non-terminal was updated to allow for empty text.
    
    We leniently re-interpret this as also allowing us to drop the trailing
    SP char after "[resp-text-code parsable code data]", like so:
      resp-text       = ["[" resp-text-code "]" [SP [text]] / [text]
    
    Also:
    ♻️ Add Patterns module, to organize regexps.
    ♻️ Use Patterns::CharClassSubtraction refinement to simplify exceptions.
    ♻️ Add ParserUtils::Generator#def_char_matchers to define SP, LBRA, RBRA.
    ♻️ Add ParserUtils#{match,accept}_re to replace TEXT, CTEXT lex_states.
    ♻️ Remove unused lex_state kwarg from match

--- lib/net/imap/response_parser/parser_utils.rb
+++ lib/net/imap/response_parser/parser_utils.rb
@@ -8,26 +8,58 @@ module Net
       # (internal API, subject to change)
       module ParserUtils # :nodoc:
 
+        module Generator
+
+          LOOKAHEAD = "(@token ||= next_token)"
+          SHIFT_TOKEN = "(@token = nil)"
+
+          # we can skip lexer for single character matches, as a shortcut
+          def def_char_matchers(name, char, token)
+            match_name = name.match(/\A[A-Z]/) ? "#{name}!" : name
+            char = char.dump
+            class_eval <<~RUBY, __FILE__, __LINE__ + 1
+              # frozen_string_literal: true
+
+              # like accept(token_symbols); returns token or nil
+              def #{name}?
+                if @token&.symbol == #{token}
+                  #{SHIFT_TOKEN}
+                  #{char}
+                elsif !@token && @str[@pos] == #{char}
+                  @pos += 1
+                  #{char}
+                end
+              end
+
+              # like match(token_symbols); returns token or raises parse_error
+              def #{match_name}
+                if @token&.symbol == #{token}
+                  #{SHIFT_TOKEN}
+                  #{char}
+                elsif !@token && @str[@pos] == #{char}
+                  @pos += 1
+                  #{char}
+                else
+                  parse_error("unexpected %s (expected %p)",
+                              @token&.symbol || @str[@pos].inspect, #{char})
+                end
+              end
+            RUBY
+          end
+
+        end
+
         private
 
-        def match(*args, lex_state: @lex_state)
-          if @token && lex_state != @lex_state
-            parse_error("invalid lex_state change to %s with unconsumed token",
-                        lex_state)
+        def match(*args)
+          token = lookahead
+          unless args.include?(token.symbol)
+            parse_error('unexpected token %s (expected %s)',
+                        token.symbol.id2name,
+                        args.collect {|i| i.id2name}.join(" or "))
           end
-          begin
-            @lex_state, original_lex_state = lex_state, @lex_state
-            token = lookahead
-            unless args.include?(token.symbol)
-              parse_error('unexpected token %s (expected %s)',
-                          token.symbol.id2name,
-                          args.collect {|i| i.id2name}.join(" or "))
-            end
-            shift_token
-            return token
-          ensure
-            @lex_state = original_lex_state
-          end
+          shift_token
+          token
         end
 
         # like match, but does not raise error on failure.
@@ -42,6 +74,14 @@ module Net
           end
         end
 
+        # To be used conditionally:
+        #   assert_no_lookahead if Net::IMAP.debug
+        def assert_no_lookahead
+          @token.nil? or
+            parse_error("assertion failed: expected @token.nil?, actual %s: %p",
+                        @token.symbol, @token.value)
+        end
+
         # like accept, without consuming the token
         def lookahead?(*symbols)
           @token if symbols.include?((@token ||= next_token)&.symbol)
@@ -49,6 +89,22 @@ module Net
 
         def lookahead
           @token ||= next_token
+        end
+
+        def accept_re(re)
+          assert_no_lookahead if Net::IMAP.debug
+          re.match(@str, @pos) and @pos = $~.end(0)
+          $~
+        end
+
+        def match_re(re, name)
+          assert_no_lookahead if Net::IMAP.debug
+          if re.match(@str, @pos)
+            @pos = $~.end(0)
+            $~
+          else
+            parse_error("invalid #{name}")
+          end
         end
 
         def shift_token
