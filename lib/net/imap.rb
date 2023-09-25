146ad376050b3ae684eb49cf2245a8df1f4afd47
nick evans <nicholas.evans@gmail.com>
2023-09-25 10:06:49 -0400

    🔖 Update VERSION to 0.4.0-dev
    I should have done this immediately after branching v0.3-bugfixes!

--- lib/net/imap.rb
+++ lib/net/imap.rb
@@ -662,7 +662,7 @@ module Net
   # * {IMAP URLAUTH Authorization Mechanism Registry}[https://www.iana.org/assignments/urlauth-authorization-mechanism-registry/urlauth-authorization-mechanism-registry.xhtml]
   #
   class IMAP < Protocol
-    VERSION = "0.3.4"
+    VERSION = "0.4.0-dev"
 
     # Aliases for supported capabilities, to be used with the #enable command.
     ENABLE_ALIASES = {
