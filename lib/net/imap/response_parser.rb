0ea6aaed09189201627cf4feb640eda67c61908a
nicholas evans <nicholas.evans@gmail.com>
2023-10-22 22:12:55 -0400

    ⚡️ Convert `msg-att` case stmt to use strings
    In my benchmarks, this leads to a measurable performance boost.
    
    To help avoid conflicts later, they were sorted like so: very common
    `UID` and `FLAGS`.  Next, in both 3051 and 9051, sorted alphabetically.
    Then, in 3051 but not 9051.  Then in 9051 but not 3501.  Then all other
    extensions, sorted alphabetically (more or less)

--- lib/net/imap/response_parser.rb
+++ lib/net/imap/response_parser.rb
@@ -655,41 +655,31 @@ module Net
       #   msg-att-static   =/ "BINARY" section-binary ["<" number ">"] SP
       #                       (nstring / literal8)
       def msg_att(n)
-        match(T_LPAR)
+        lpar
         attr = {}
         while true
-          token = lookahead
-          case token.symbol
-          when T_RPAR
-            shift_token
-            break
-          when T_SPACE
-            shift_token
-            next
-          end
-          case token.value
-          when /\A(?:ENVELOPE)\z/ni
-            name, val = envelope_data
-          when /\A(?:FLAGS)\z/ni
-            name, val = flags_data
-          when /\A(?:INTERNALDATE)\z/ni
-            name, val = internaldate_data
-          when /\A(?:RFC822(?:\.HEADER|\.TEXT)?)\z/ni
-            name, val = rfc822_text
-          when /\A(?:RFC822\.SIZE)\z/ni
-            name, val = rfc822_size
-          when /\A(?:BODY(?:STRUCTURE)?)\z/ni
-            name, val = body_data
-          when /\A(?:UID)\z/ni
-            name, val = uid_data
-          when /\A(?:MODSEQ)\z/ni
-            name, val = modseq_data
-          else
-            parse_error("unknown attribute `%s' for {%d}", token.value, n)
-          end
+          name = lookahead!(T_ATOM).value.upcase
+          name, val =
+            case name
+            when "UID"                  then uid_data
+            when "FLAGS"                then flags_data
+            when "BODY"                 then body_data
+            when "BODYSTRUCTURE"        then body_data
+            when "ENVELOPE"             then envelope_data
+            when "INTERNALDATE"         then internaldate_data
+            when "RFC822.SIZE"          then rfc822_size
+            when "RFC822"               then rfc822_text
+            when "RFC822.HEADER"        then rfc822_text        # not in rev2
+            when "RFC822.TEXT"          then rfc822_text        # not in rev2
+            when "MODSEQ"               then modseq_data        # CONDSTORE
+            else parse_error("unknown attribute `%s' for {%d}", name, n)
+            end
           attr[name] = val
+          break unless SP?
+          break if lookahead_rpar?
         end
-        return attr
+        rpar
+        attr
       end
 
       def envelope_data
