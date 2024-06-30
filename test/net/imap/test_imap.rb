221911ee2c850acef7b05a603c1d51722cbccb91
nick evans <nick@rubinick.dev>
2024-06-30 14:49:31 -0400

    🥅 Cancel AUTHENTICATE if process raises an error [🚧 server error]
    The exception will be re-raised after the protocol cancel response has
    been sent.

--- test/net/imap/test_imap.rb
+++ test/net/imap/test_imap.rb
@@ -1073,6 +1073,25 @@ EOF
     end
   end
 
+  test "#authenticate can be canceled with SASL::AuthenticationCanceled" do
+    with_fake_server(
+      preauth: false, cleartext_auth: true,
+      sasl_ir: false, sasl_mechanisms: %i[PLAIN]
+    ) do |server, imap|
+      registry = Net::IMAP::SASL::Authenticators.new(use_defaults: false)
+      registry.add_authenticator :plain, ->(*a, **kw, &b) {
+        ->(challenge) {
+          raise(Net::IMAP::SASL::AuthenticationCanceled,
+                "a: %p, kw: %p, b: %p" % [a, kw, b])
+        }
+      }
+      assert_raise_with_message(Net::IMAP::BadResponseError, "canceled") do
+        imap.authenticate(:plain, hello: :world, registry: registry)
+      end
+      refute imap.disconnected?
+    end
+  end
+
   test "#uid_expunge with EXPUNGE responses" do
     with_fake_server(select: "INBOX",
                      extensions: %i[UIDPLUS]) do |server, imap|
