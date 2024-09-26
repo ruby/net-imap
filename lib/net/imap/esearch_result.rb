cd1a3645bf6a3b0b16dda20db84bdc89e4c8fa9e
nick evans <nick@rubinick.dev>
2024-09-26 17:31:44 -0400

    ♻️ Convert ESearchResult to ruby 3.2 Data polyfill

--- lib/net/imap/esearch_result.rb
+++ lib/net/imap/esearch_result.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative "data_lite"
+
 module Net
   class IMAP
     # An Extended search result which is returned by IMAP#search,
@@ -12,49 +14,12 @@ module Net
     # * The server supports +IMAP4rev2+ but _not_ +IMAP4rev1+.
     # * +IMAP4rev2+ has been enabled.
     #
-    class ESearchResult
+    class ESearchResult < DataLite
       def self.members; %i[tag uid data].freeze end
-
-      def self.new(*args, **kwargs, &block)
-        if args.any?
-          if args.size > members.size
-            raise ArgumentError, "unknown arguments #{new_args[members.size..].join(', ')}"
-          end
-          kwargs = Hash[members.take(args.size).zip(args)]
-        end
-        allocate.tap do |instance|
-          instance.send(:initialize, **kwargs, &block)
-        end.freeze
-      end
 
       def initialize(tag: nil, uid: nil, data: nil)
         @tag, @uid, @data = tag, uid, data
         freeze
-      end
-
-      def members;     self.class.members                    end
-      def attributes;  Hash[members.map {|m| [m, send(m)] }] end
-      def to_h(&block) attributes.to_h(&block)               end
-      def hash;        to_h.hash                             end
-      def deconstruct; attributes.values                     end
-
-      def deconstruct_keys(keys)
-        raise TypeError unless keys.is_a?(Array) || keys.nil?
-        return attributes if keys&.first.nil?
-        attributes.slice(*keys)
-      end
-
-      def ==(other)
-        self.class == other.class && to_h == other.to_h
-      end
-
-      def eql?(other)
-        self.class == other.class && hash == other.hash
-      end
-
-      def with(**kwargs)
-        return self if kwargs.empty?
-        self.class.new(**attributes.merge(kwargs))
       end
 
       # :call-seq: to_a -> Array of integers
@@ -226,12 +191,6 @@ module Net
       # See <tt>SEARCH=FUZZY</tt>
       # {[RFC6203]}[https://www.rfc-editor.org/rfc/rfc6203.html]
       def relevancy;  data.assoc("RELEVANCY")&.last  end
-
-      private
-
-      def initialize_copy(source)
-        super.freeze
-      end
 
     end
   end
