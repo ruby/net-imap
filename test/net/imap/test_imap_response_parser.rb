1082b22846cf6a8b863ba621ef8a311db1543de4
nick evans <nick@rubinick.dev>
2025-01-23 11:04:12 -0500

    🚚 Move "response data" tests to appropriate files

--- test/net/imap/test_imap_response_parser.rb
+++ test/net/imap/test_imap_response_parser.rb
@@ -193,4 +193,25 @@ class IMAPResponseParserTest < Test::Unit::TestCase
     Net::IMAP.debug = debug
   end
 
+  test "COPYUID with backwards ranges" do
+    parser = Net::IMAP::ResponseParser.new
+    response = parser.parse(
+      "A004 OK [copyUID 9999 20:19,500:495 92:97,101:100] Done\r\n"
+    )
+    code = response.data.code
+    assert_equal(
+      {
+         19 =>  92,
+         20 =>  93,
+        495 =>  94,
+        496 =>  95,
+        497 =>  96,
+        498 =>  97,
+        499 => 100,
+        500 => 101,
+      },
+      code.data.uid_mapping
+    )
+  end
+
 end
