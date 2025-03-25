936ecde3bc2e0446194bf76c231ac562a708c370
nick evans <nick@rubinick.dev>
2025-03-25 13:08:21 -0400

    ♻️ Anchor literal regexp to the end of the buffer
    This also allows us to check against the concatenated buffer, rather
    than the smaller line buffer.  That distinction doesn't really matter
    now, since we always read an entire line at once.  But it will matter if
    we read partial lines.

--- lib/net/imap.rb
+++ lib/net/imap.rb
@@ -1300,7 +1300,7 @@ module Net
         s = @sock.gets(CRLF)
         break unless s
         buff.concat(s)
-        if /\{(\d+)\}\r\n/n =~ s
+        if /\{(\d+)\}\r\n\z/n =~ buff
           s = @sock.read($1.to_i)
           buff.concat(s)
         else
