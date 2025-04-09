e0059251e854cb03d5209c682ba3484fcb6953cd
nick evans <nick@rubinick.dev>
2025-04-09 09:54:51 -0400

    ✅ Fix backport to not-imap 0.3 and ruby 2.6
    For the net-imap v0.3 backport, two major changes were needed:
    * the tests needed to be almost completely rewritten because FakeServer
      was added for v0.4.
    * `max_response_size` needed to be on Net::IMAP directly, because Config
      was added for v0.4.

--- test/net/imap/test_imap_max_response_size.rb
+++ test/net/imap/test_imap_max_response_size.rb
@@ -2,13 +2,10 @@
 
 require "net/imap"
 require "test/unit"
-require_relative "fake_server"
 
 class IMAPMaxResponseSizeTest < Test::Unit::TestCase
-  include Net::IMAP::FakeServer::TestHelper
 
   def setup
-    Net::IMAP.config.reset
     @do_not_reverse_lookup = Socket.do_not_reverse_lookup
     Socket.do_not_reverse_lookup = true
     @threads = []
@@ -23,45 +20,95 @@ class IMAPMaxResponseSizeTest < Test::Unit::TestCase
   end
 
   test "#max_response_size reading literals" do
-    with_fake_server(preauth: true) do |server, imap|
+    _, port = with_server_socket do |sock|
+      sock.gets # => NOOP
+      sock.print("RUBY0001 OK done\r\n")
+      sock.gets # => NOOP
+      sock.print("* 1 FETCH (BODY[] {12345}\r\n" + "a" * 12_345 + ")\r\n")
+      sock.print("RUBY0002 OK done\r\n")
+      "RUBY0003"
+    end
+    Timeout.timeout(5) do
+      imap = Net::IMAP.new("localhost", port: port, max_response_size: 640 << 20)
+      assert_equal 640 << 20, imap.max_response_size
       imap.max_response_size = 12_345 + 30
-      server.on("NOOP") do |resp|
-        resp.untagged("1 FETCH (BODY[] {12345}\r\n" + "a" * 12_345 + ")")
-        resp.done_ok
-      end
-      imap.noop
-      assert_equal "a" * 12_345, imap.responses("FETCH").first.message
+      assert_equal 12_345 + 30, imap.max_response_size
+      imap.noop # to reset the get_response limit
+      imap.noop # to send the FETCH
+      assert_equal "a" * 12_345, imap.responses["FETCH"].first.attr["BODY[]"]
+    ensure
+      imap.logout rescue nil
+      imap.disconnect rescue nil
     end
   end
 
   test "#max_response_size closes connection for too long line" do
-    Net::IMAP.config.max_response_size = 10
-    run_fake_server_in_thread(preauth: false, ignore_io_error: true) do |server|
-      assert_raise_with_message(
-        Net::IMAP::ResponseTooLargeError, /exceeds max_response_size .*\b10B\b/
-      ) do
-        with_client("localhost", port: server.port) do
-          fail "should not get here (greeting longer than max_response_size)"
-        end
-      end
+    _, port = with_server_socket do |sock|
+      sock.gets or next # => never called
+      fail "client disconnects first"
+    end
+    assert_raise_with_message(
+      Net::IMAP::ResponseTooLargeError, /exceeds max_response_size .*\b10B\b/
+    ) do
+      Net::IMAP.new("localhost", port: port, max_response_size: 10)
+      fail "should not get here (greeting longer than max_response_size)"
     end
   end
 
   test "#max_response_size closes connection for too long literal" do
-    Net::IMAP.config.max_response_size = 1<<20
-    with_fake_server(preauth: false, ignore_io_error: true) do |server, client|
-      client.max_response_size = 50
-      server.on("NOOP") do |resp|
-        resp.untagged("1 FETCH (BODY[] {1000}\r\n" + "a" * 1000 + ")")
-      end
-      assert_raise_with_message(
-        Net::IMAP::ResponseTooLargeError,
-        /\d+B read \+ 1000B literal.* exceeds max_response_size .*\b50B\b/
-      ) do
-        client.noop
-        fail "should not get here (FETCH literal longer than max_response_size)"
-      end
+    _, port = with_server_socket(ignore_io_error: true) do |sock|
+      sock.gets # => NOOP
+      sock.print "* 1 FETCH (BODY[] {1000}\r\n" + "a" * 1000 + ")\r\n"
+      sock.print("RUBY0001 OK done\r\n")
+    end
+    client = Net::IMAP.new("localhost", port: port, max_response_size: 1000)
+    assert_equal 1000, client.max_response_size
+    client.max_response_size = 50
+    assert_equal 50, client.max_response_size
+    assert_raise_with_message(
+      Net::IMAP::ResponseTooLargeError,
+      /\d+B read \+ 1000B literal.* exceeds max_response_size .*\b50B\b/
+    ) do
+      client.noop
+      fail "should not get here (FETCH literal longer than max_response_size)"
     end
   end
 
+  def with_server_socket(ignore_io_error: false)
+    server = create_tcp_server
+    port   = server.addr[1]
+    start_server do
+      Timeout.timeout(5) do
+        sock = server.accept
+        sock.print("* OK connection established\r\n")
+        logout_tag = yield sock if block_given?
+        sock.gets # => LOGOUT
+        sock.print("* BYE terminating connection\r\n")
+        sock.print("#{logout_tag} OK LOGOUT completed\r\n") if logout_tag
+      rescue IOError, EOFError, Errno::ECONNABORTED, Errno::ECONNRESET,
+        Errno::EPIPE, Errno::ETIMEDOUT
+        ignore_io_error or raise
+      ensure
+        sock.close rescue nil
+        server.close rescue nil
+      end
+    end
+    return server, port
+  end
+
+  def start_server
+    th = Thread.new do
+      yield
+    end
+    @threads << th
+    sleep 0.1 until th.stop?
+  end
+
+  def create_tcp_server
+    return TCPServer.new(server_addr, 0)
+  end
+
+  def server_addr
+    Addrinfo.tcp("localhost", 0).ip_address
+  end
 end
