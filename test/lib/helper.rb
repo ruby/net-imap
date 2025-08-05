aba68a74b5f5c989fae37cfe9cf5db6bc3b341c3
nick evans <nick@rubinick.dev>
2025-08-05 14:36:01 -0400

    ✅ Disable simplecov with environment variable

--- test/lib/helper.rb
+++ test/lib/helper.rb
@@ -1,31 +1,34 @@
-require "simplecov"
+unless ENV["SIMPLECOV_DISABLE"] in /\A(1|y(es)?|t(rue)?)\z/i
+  require "simplecov"
 
-# Cannot use ".simplecov" file: simplecov-json triggers a circular require.
-require "simplecov-json"
-SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::HTMLFormatter,
-  SimpleCov::Formatter::JSONFormatter,
-])
+  # Cannot use ".simplecov" file: simplecov-json triggers a circular require.
+  require "simplecov-json"
+  SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([
+    SimpleCov::Formatter::HTMLFormatter,
+    SimpleCov::Formatter::JSONFormatter,
+  ])
 
-SimpleCov.start do
-  command_name "Net::IMAP tests"
-  enable_coverage  :branch
-  primary_coverage :branch
-  enable_coverage_for_eval
+  SimpleCov.start do
+    command_name "Net::IMAP tests"
+    enable_coverage  :branch
+    primary_coverage :branch
+    enable_coverage_for_eval
 
-  add_filter "/test/"
-  add_filter "/rakelib/"
+    add_filter "/test/"
+    add_filter "/rakelib/"
 
-  add_group "Parser", %w[lib/net/imap/response_parser.rb
-                         lib/net/imap/response_parser]
-  add_group "Config", %w[lib/net/imap/config.rb
-                         lib/net/imap/config]
-  add_group "SASL", %w[lib/net/imap/sasl.rb
-                       lib/net/imap/sasl
-                       lib/net/imap/authenticators.rb]
-  add_group "StringPrep", %w[lib/net/imap/stringprep.rb
-                             lib/net/imap/stringprep]
+    add_group "Parser", %w[lib/net/imap/response_parser.rb
+                          lib/net/imap/response_parser]
+    add_group "Config", %w[lib/net/imap/config.rb
+                          lib/net/imap/config]
+    add_group "SASL", %w[lib/net/imap/sasl.rb
+                        lib/net/imap/sasl
+                        lib/net/imap/authenticators.rb]
+    add_group "StringPrep", %w[lib/net/imap/stringprep.rb
+                              lib/net/imap/stringprep]
+  end
 end
+
 require "test/unit"
 require "core_assertions"
 
