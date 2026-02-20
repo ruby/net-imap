48476f68a01d331e83b44d31871c405b76bfdfa6
nick evans <nick@rubinick.dev>
2026-02-20 09:39:29 -0500

    ✅🚚 Move `#append` tests to their own file

---
+++ test/net/imap/test_imap_append.rb
@@ -0,0 +1,115 @@
+# frozen_string_literal: true
+
+require "net/imap"
+require "test/unit"
+require_relative "fake_server"
+
+class IMAPAppendTest < Net::IMAP::TestCase
+  include Net::IMAP::FakeServer::TestHelper
+
+  def test_append
+    server = create_tcp_server
+    port = server.addr[1]
+    mail = <<EOF.gsub(/\n/, "\r\n")
+From: shugo@example.com
+To: matz@example.com
+Subject: hello
+
+hello world
+EOF
+    requests = []
+    received_mail = nil
+    start_server do
+      sock = server.accept
+      begin
+        sock.print("* OK test server\r\n")
+        line = sock.gets
+        requests.push(line)
+        size = line.slice(/{(\d+)}\r\n/, 1).to_i
+        sock.print("+ Ready for literal data\r\n")
+        received_mail = sock.read(size)
+        sock.gets
+        sock.print("RUBY0001 OK APPEND completed\r\n")
+        requests.push(sock.gets)
+        sock.print("* BYE terminating connection\r\n")
+        sock.print("RUBY0002 OK LOGOUT completed\r\n")
+      ensure
+        sock.close
+        server.close
+      end
+    end
+
+    begin
+      imap = Net::IMAP.new(server_addr, :port => port)
+      imap.append("INBOX", mail)
+      assert_equal(1, requests.length)
+      assert_equal("RUBY0001 APPEND INBOX {#{mail.size}}\r\n", requests[0])
+      assert_equal(mail, received_mail)
+      imap.logout
+      assert_equal(2, requests.length)
+      assert_equal("RUBY0002 LOGOUT\r\n", requests[1])
+    ensure
+      imap.disconnect if imap
+    end
+  end
+
+  def test_append_fail
+    server = create_tcp_server
+    port = server.addr[1]
+    mail = <<EOF.gsub(/\n/, "\r\n")
+From: shugo@example.com
+To: matz@example.com
+Subject: hello
+
+hello world
+EOF
+    requests = []
+    start_server do
+      sock = server.accept
+      begin
+        sock.print("* OK test server\r\n")
+        requests.push(sock.gets)
+        sock.print("RUBY0001 NO Mailbox doesn't exist\r\n")
+        requests.push(sock.gets)
+        sock.print("* BYE terminating connection\r\n")
+        sock.print("RUBY0002 OK LOGOUT completed\r\n")
+      ensure
+        sock.close
+        server.close
+      end
+    end
+
+    begin
+      imap = Net::IMAP.new(server_addr, :port => port)
+      assert_raise(Net::IMAP::NoResponseError) do
+        imap.append("INBOX", mail)
+      end
+      assert_equal(1, requests.length)
+      assert_equal("RUBY0001 APPEND INBOX {#{mail.size}}\r\n", requests[0])
+      imap.logout
+      assert_equal(2, requests.length)
+      assert_equal("RUBY0002 LOGOUT\r\n", requests[1])
+    ensure
+      imap.disconnect if imap
+    end
+  end
+
+  private
+
+  def start_server
+    th = Thread.new do
+      yield
+    end
+    @threads << th
+    sleep 0.1 until th.stop?
+  end
+
+  def create_tcp_server
+    return TCPServer.new(server_addr, 0)
+  end
+
+  def server_addr
+    Addrinfo.tcp("localhost", 0).ip_address
+  end
+
+end
