7cc1dd0a11a4f5faeb5c92676f78e348a0104a87
nick evans <nick@rubinick.dev>
2026-07-23 15:44:10 -0400

    🔖 Bump version to 0.6.6

--- lib/net/imap.rb
+++ lib/net/imap.rb
@@ -819,7 +819,7 @@ module Net
   # * {IMAP URLAUTH Authorization Mechanism Registry}[https://www.iana.org/assignments/urlauth-authorization-mechanism-registry/urlauth-authorization-mechanism-registry.xhtml]
   #
   class IMAP < Protocol
-    VERSION = "0.6.5"
+    VERSION = "0.6.6"
 
     # Aliases for supported capabilities, to be used with the #enable command.
     ENABLE_ALIASES = {
