ruby/net-imap

commit a742a1099fd4689d4c8a52db4a3d2ab1be10ae8c

♻️ Extract regexp constants in send_string_data
These are identical regexps, make the code better self-documenting.

Note that ASTRING_SPECIALS is identical to
ResponseParser::Patterns::ASTRING_SPECIALS.

lib/net/imap/command_data.rb
@@ -55,6 +55,10 @@ module Net
       end
     end
 
+    UNQUOTABLE_CHARS = /\r\n/n
+    ASTRING_SPECIALS = /[(){ \x00-\x1f\x7f%*"\\]/n
+    private_constant :UNQUOTABLE_CHARS, :ASTRING_SPECIALS
+
     # Sends generic strings formatted as +astring+:
     # * as an atom (note: this is based on +ASTRING-CHAR+, not +ATOM-CHAR+)
     # * as a quoted string
@@ -65,7 +69,7 @@ module Net
       if str.empty?
         # same as send_quoted_string(str), but incompatible encoding is allowed
         put_string('""')
-      elsif str.match?(/[\r\n]/n)
+      elsif str.match?(UNQUOTABLE_CHARS)
         send_literal(str, tag)
       elsif !str.ascii_only?
         if @utf8_strings
@@ -73,7 +77,7 @@ module Net
         else
           send_literal(str, tag)
         end
-      elsif str.match?(/[(){ \x00-\x1f\x7f%*"\\]/n)
+      elsif str.match?(ASTRING_SPECIALS)
         send_quoted_string(str)
       else
         # valid +astring+ atom: non-empty, ASCII only, no ASTRING_SPECIALS

commit c6595fe3201ae8963ec8636c5531a2352ca86014

🐛 Fix bug in #712, testing if string is quotable
This bug was introduced by a742a109.  When I extracted the
`UNQUOTABLE_CHARS` regexp into a constant, I didn't copied it exactly.
It was converted from a character class into a sequence of bytes:
```ruby
    # It should've been this:
    UNQUOTABLE_CHARS = /[\0\r\n]/n
    # But I incorrectly copied it as this:
    UNQUOTABLE_CHARS = /\0\r\n/n
```

If that regexp were the only guard, this regexp would've allowed CRLF
injections!  Fortunately, quoted strings are now sent using
`Net::IMAP::QuotedString` (since #698, released in v0.6.4.1), and that
_does_ correctly validate that the input is a valid quoted string.  So,
the string isn't _sent_ as invalid quoted data.

Unfortunately, this does result in an exception for strings that could
be valid literals.  And, because it crashes while sending command
arguments, rather than during validation, the connection will be broken.

lib/net/imap/command_data.rb
@@ -58,7 +58,7 @@ module Net
       end
     end
 
-    UNQUOTABLE_CHARS = /\0\r\n/
+    UNQUOTABLE_CHARS = /[\0\r\n]/
     ASTRING_SPECIALS = /[(){ \x00-\x1f\x7f%*"\\]/
     private_constant :UNQUOTABLE_CHARS, :ASTRING_SPECIALS
 

test/net/imap/test_imap.rb
@@ -1045,6 +1045,10 @@ class IMAPTest < Net::IMAP::TestCase
       send_args.call ["\xDE\xAD\xBE\xEF".b]
       assert_equal "({4}\r\n\xDE\xAD\xBE\xEF)".b, server.commands.pop.args
 
+      send_args.call ["hi\rthere\n", "huh?\r\nfake out"]
+      assert_equal "({9}\r\nhi\rthere\n {14}\r\nhuh?\r\nfake out)".b,
+                   server.commands.pop.args
+
       # enable automatic non-synchronizing literals
       imap.config.max_non_synchronizing_literal = 1024
       buff = bytes = nil